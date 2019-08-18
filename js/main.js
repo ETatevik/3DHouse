8a13ac4e210bd23a257b8ef2e7dc43c98eb2174b
Yegiazaryan Tatevik <barev.tatevik@gmail.com>
2019-08-19 00:23:13 +0400

    Go 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function($) {
 	
+	// $('#projMiniMenuBody li > a').click(function(event) {
+	// 	$('body').removeClass('modal-open');
+	// 	$('#projMenuModal').removeClass('show').attr('aria-modal', 'false').css('display', 'none');
+	// 	$('.modal-backdrop').remove();
+	// });
 	
+
 	
 });
