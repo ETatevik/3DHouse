276e02400385ebd8ec661fd17a3d1ba32eb91e77
Yegiazaryan Tatevik <barev.tatevik@gmail.com>
2019-08-21 03:34:32 +0400

    Home page : In the end is here, but range wtf

--- js/main.js
+++ js/main.js
@@ -11,36 +11,93 @@ jQuery(document).ready(function($) {
 
 	//  Filter Find of Projects
 	{
-		$("#filterFindHome").css('top', `${$('#menu3D').height()}px`);
-		if($(window).width() > 1000){
+		// $("#filterFindHome").css('top', `${$('#menu3D').height()}px`);
+		let galleryW;
+		if($(window).width() > 1200) $('#filterFindHome').css('top', `${$('#menu3D').height()}px`);
+
+		if($(window).width() > 1100){
+			galleryW =$('#bigGalleryHome3d').width() - $('#filterFindHome').width();
 			$(".filterHomeBody").width(`${$("#menu3D > .container-fluid > .navbar > .minMenu3D > .row").width()}px`);
-		}else{
-			$(".filterHomeBody").width(`calc(${$("#menu3D > .container-fluid > .navbar > .minMenu3D > .row").width() - 2}px - ${$('.filterHomeBtn').width()}px)`);
+			$('.filt3Dhei').width(`${galleryW}px`).css({marginLeft : '0', marginRight : 'auto'});
+			$('#bigGalleryHome3d').width(`${galleryW}px`);
 		}
-		$("#filterFindHome").css('right', `-${$(".filterHomeBody").width()}px`);
-		setTimeout(()=>{
-			$("#filterFindHome").css('transition', 'right .5s');
-		}, 100);
+		// else if($(window).width() < 1000 && $(window).width() > 800){
+		// 	$(".filterHomeBody").width(`calc(${2*$('#bigGalleryHome3d').width()/3}px - ${$('.filterHomeBtn').width()}px`);
+		// 	$('#bigGalleryHome3d').width(`${galleryW}px`);
+		// 	$('.filt3Dhei').css({width:'100%', marginRight: 'auto', marginLeft:'auto'});
+		// }
+		else{
+			galleryW = $('#bigGalleryHome3d').width() - $('.filterHomeBtn').width()*1.1;
+			$(".filterHomeBody").width(galleryW);
+			$('#bigGalleryHome3d').width(`${0}px`).css('overflow', 'hidden');
+			$('.filt3Dhei').css({width:'100%', marginRight: 'auto', marginLeft:'auto'});		
+		}
+
+
+		// let pageHeight = 0;
+		// let startHei =  $('.filt3Dhei').eq(0).width() + $('.filt3Dhei').eq(1).width();
+		// let filterStartHei = $('#filterFindHome .findBox .filterHomeBody ').height() + $('#menu3D').height();
+
+		// if(filterStartHei >= $('.filt3Dhei').eq(0).width()+$('#menu3D').height() && filterStartHei <= startHei){
+		// 	$('.filt3Dhei').eq(1).children('.container').width(`${galleryW - 100}px`).css({marginLeft : '40px', marginRight : 'auto'});
+		// }
+
 
 		$("#filterFindHome .findBox .filterHomeBody dl > dd").slideUp();
 		$("#filterFindHome .findBox .filterHomeBody dl > dt").on('click', function(event) {
 			event.preventDefault();
 			$(this).next('dd').slideToggle(500);
 			$(this).children(".plus").text() == '+' ? $(this).children(".plus").text('-'):$(this).children(".plus").text('+');
+			// filterStartHei = $('#filterFindHome .findBox .filterHomeBody ').height() + $('#menu3D').height();
+
+			// pageHeight = 0;
+			// startHei = 0;
+			// $('.filt3Dhei').each(function(index, el) {
+			// 		startHei += pageHeight;
+			// 		pageHeight += $(this).width();
+			// 		console.log(pageHeight);
+
+			// 		if(filterStartHei >= startHei + $('#menu3D').height() && filterStartHei <= pageHeight){
+			// 			$(this).children('.container').width(`${galleryW - 100}px`).css({marginLeft : '40px', marginRight : 'auto'});
+			// 			console.log('aaa');
+			// 		}
+			// });
+			
 		});
 
 
-		let flagFilterBtn = false;
+		let flagFilterBtn = true;
+		$('.filterHomeBtn').children('span').text('Скрыть');
 		$('.filterHomeBtn').on('click',  function(event) {
 			event.preventDefault();
 			if(!flagFilterBtn){
 				flagFilterBtn = true;
-				$("#filterFindHome").css('right', 0);
+				// $("#filterFindHome").css('right', 0);
 				$(this).children('span').text('Скрыть');
+				if($(window).width() > 1100){
+					$('.filt3Dhei').width(`${galleryW}px`).css({marginLeft : '0', marginRight : 'auto'});
+					$('#bigGalleryHome3d').width(`${galleryW}px`);
+					$(".filterHomeBody").width(`${$("#menu3D > .container-fluid > .navbar > .minMenu3D > .row").width()}px`).css('overflow', 'visibility');
+				}else
+				// else if($(window).width() < 800)
+				{
+					$('#bigGalleryHome3d').width(`${0}px`).css('overflow', 'hidden');
+					$(".filterHomeBody").width(galleryW).css('overflow', 'visibility');
+				}
+
 			}else{
 				flagFilterBtn = false;
-				$("#filterFindHome").css('right', `-${$(".filterHomeBody").width()}px`);
+				// $("#filterFindHome").css('right', `-${$(".filterHomeBody").width()}px`);
 				$(this).children('span').text('Открыть фильтр');
+				$('#bigGalleryHome3d').width(`100vw`);
+				$(".filterHomeBody").width(0).css('overflow', 'hidden');
+				if($(window).width() > 1100){
+					$('.filt3Dhei').width(`100%`).css({marginLeft : 'auto', marginRight : 'auto'});
+				}
+				if($(window).width() < 800){
+					$('#bigGalleryHome3d').css('overflow', 'visibility');
+
+				}
 			}
 		});
 	}
@@ -62,10 +119,7 @@ jQuery(document).ready(function($) {
 		});
 	}
 
-	// 
-	{
-		// 
-	}
+	
 
 	
 });
